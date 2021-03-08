38d581cb38f14846d113f11f903297973378c08e
Hongjin Zhu <summerellaooo@gmail.com>
2021-03-09 03:11:26 +0800

    export the function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,17 +1,8 @@
-const eqArrays = function(array1, array2) {
-  if(array1.length !== array2.length){
-    return false;
-  }else{
-    for(let i = 0; i < array1.length; i++){
-      if (array1[i] !== array2[i]){
-        return false;
-      }
-    }
-  }
-  return true;
-}
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(a1, a2){
   const result = eqArrays(a1, a2);
   console.log(result);
 }
+
+module.exports = assertArraysEqual;
