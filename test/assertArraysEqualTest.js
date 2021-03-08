641762d407e4a67228b0f4ac7c6f16fdf9bac341
Hongjin Zhu <summerellaooo@gmail.com>
2021-03-09 03:10:45 +0800

    test file

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,8 @@
+
+const assertArraysEqual = require('../assertArraysEqual');
+const eqArrays = require('../eqArrays');
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
